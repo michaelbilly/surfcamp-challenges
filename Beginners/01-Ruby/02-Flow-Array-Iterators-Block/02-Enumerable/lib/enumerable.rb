f980faec6e700d96f975ba76daf52408119588a5
Michael Billy <mixbilly@gmail.com>
2014-09-11 10:38:19 +0200

    tutorat express par boris

--- Beginners/01-Ruby/02-Flow-Array-Iterators-Block/02-Enumerable/lib/enumerable.rb
+++ Beginners/01-Ruby/02-Flow-Array-Iterators-Block/02-Enumerable/lib/enumerable.rb
@@ -1,10 +1,10 @@
 def sum_odd_indexed(liste)
   sum = 0
-  liste.each_with_index { |number, index|
-      if index.odd?
-          sum = sum + number
-      end
-    }
+  liste.each_with_index do |number, index|
+    if index.odd?
+        sum = sum + number
+    end
+  end
   return sum
   # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.) 
   #       You should make use Enumerable#each_with_index
@@ -14,33 +14,43 @@ tab =[2,4,8,5,9,0,3,6,7,1,5,7,3]
 p sum_odd_indexed(tab)
  
 def even_numbers(array)
-  even_tab = array.select do |integer| 
-      integer.even? 
-    end
-    return even_tab
+  array.select do |integer| 
+    integer.even? 
+  end
   # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
 end
 
+
 tab = [3,5,2,6,9,7,1]
 p even_numbers(tab)
+
+
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  array.reject do |word|
+    word.size > max_length
+  end
 end
  
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find do |number|
+    number < limit
+  end
 end
  
 def add_bang(array)
-  new_array = array.map do |str|
-    "#{str} !"
-    return = new_array
+  array.map {|str| "#{str} !" }  
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
 end
+
+add_bang(["yip", "yeah"])
+
+# ["yip !", "yeah !"]
  
 def product(array)
   # TODO: Calculate the product of an array of numbers.
