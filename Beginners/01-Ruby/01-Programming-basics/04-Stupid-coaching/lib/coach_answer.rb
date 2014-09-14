f0dee13eb8bd2a23eb3fe0ca0ca80a17b2ed813e
Michael Billy <mixbilly@gmail.com>
2014-09-14 23:13:18 +0200

    pas exactement ce qui est demandé

--- Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
+++ Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
@@ -1,7 +1,10 @@
 def coach_answer(your_message)
-puts "je comprends que" "#{your_message}"
-	return reponse
-  # TODO: return coach answer to your_message
+
+if your_message.end_with? "?"
+	puts "Silly question, get dressed and go to work !"
+	else puts "I don't care son, get dressed and go to work !"
+end
+# TODO: return coach answer to your_message
 end
 
 def coach_answer_enhanced(your_message)
--- Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
+++ Beginners/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,12 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "bienvenue elève, qu'est ce qui te chifonne ?"
 
+response = gets.chomp!
+
+until response == ""
+	coach_answer(response)
+	response = gets.chomp!
+end
+
+puts "allez ciao"
